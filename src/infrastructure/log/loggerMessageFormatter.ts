1769ec4c1595e4a299fa4c9d1e322bc9c7d1c6aa
luansantosdevpy <luansantosdev.py@gmail.com>
2024-12-09 17:13:10 -0300

    refactor: refactor logger message formatter

--- src/infrastructure/log/loggerMessageFormatter.ts
+++ src/infrastructure/log/loggerMessageFormatter.ts
@@ -1,23 +1,21 @@
-import type winston from 'winston';
-
 import { levels } from './loggerConfig';
 
-export const consoleMessageFormatter = (info: winston.LogEntry): string => {
+export const consoleMessageFormatter = (info: any): string => {
   let meta = '';
 
-  if (Object.keys(info.metadata).length > 0) {
+  if (info.metadata && Object.keys(info.metadata).length > 0) {
     meta = ` => ${JSON.stringify(info.metadata)}`;
   }
 
-  return `[${info.level}] ${info.timestamp} -> ${info.message}${meta}`;
+  return `[${info.level}] ${info.timestamp || ''} -> ${String(info.message)}${meta}`;
 };
 
-export const cloudWatchMessageFormatter = (info: winston.LogEntry): string => {
-  return `[${info.level.toUpperCase()}] ${info.timestamp} ${JSON.stringify({
+export const cloudWatchMessageFormatter = (info: any): string => {
+  return `[${info.level.toUpperCase()}] ${info.timestamp || ''} ${JSON.stringify({
     pid: process.pid,
     level: levels[info.level as keyof typeof levels],
-    timestamp: new Date(info.timestamp).getTime(),
-    message: info.message,
+    timestamp: info.timestamp ? new Date(info.timestamp).getTime() : undefined,
+    message: String(info.message),
     _log_type: 'application',
     extraInfo: {
       ...info.metadata
